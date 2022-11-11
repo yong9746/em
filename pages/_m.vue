e7637e8fa38832060d83964c47199cd56b0dfd0c
JeremyPoh <jeremypoh0205@gmail.com>
2022-11-11 18:07:22 +0800

    zzz

--- pages/_m.vue
+++ pages/_m.vue
@@ -19,7 +19,7 @@
     <div class="page-wrapper">
       <div class="content-wrapper menu-bg">
         <main>
-          <div class="page-container bbb">
+          <div class="page-container zzz">
             
             <section
               class="vendor-section ooo"
